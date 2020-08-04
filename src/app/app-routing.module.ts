f5d5d51c7a63d778a8f8ed57c742b38f367aa397
Mário <mario.canys@hotmail.com>
2020-08-04 16:19:06 -0300

    --

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,8 @@ import { AuthGuardService } from './core/guards/auth-guard.service';
 const routes: Routes = [
   { path: 'login', loadChildren: () => import('./modules/login/login.module').then(m => m.LoginModule) },
   { path: 'home', loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule), canActivate: [AuthGuardService] },
-  { path: '**', redirectTo: 'home', pathMatch: 'full' }
+  { path: 'pedidos', loadChildren: () => import('./modules/pedidos/pedidos.module').then(m => m.PedidosModule) },
+  { path: '**', redirectTo: 'home', pathMatch: 'full' },
 ];
 
 @NgModule({
